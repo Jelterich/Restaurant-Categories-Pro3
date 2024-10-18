6f89e74c7f820fe5f12a259ff08d761c16a42371
JeanR41 <jeanryanlozon@gmail.com>
2024-10-17 21:21:37 -0500

    testing out Observable Plot graphics

--- restaurants_cities.sql.sql
+++ restaurants_cities.sql.sql
@@ -13,17 +13,16 @@ CREATE TABLE "restaurants" (
     "city" varchar(50)   NOT NULL,
     "state" varchar(50)   NOT NULL,
     "postal_code" varchar(15)   NOT NULL,
-    "latitude" int   NOT NULL,
-    "longitude" int   NOT NULL,
-    "stars" int   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    "stars" float   NOT NULL,
     "review_count" int   NOT NULL,
     "is_open" int   NOT NULL,
     "attributes" varchar(400)   NOT NULL,
     "categories" varchar(400)   NOT NULL,
     "hours" varchar(200)   NOT NULL,
-    CONSTRAINT "pk_restaurants" PRIMARY KEY (
-        "city","state"
-     )
+    PRIMARY KEY ("city"),
+	PRIMARY KEY ("state")
 );
 
 CREATE TABLE "cities" (
@@ -33,19 +32,29 @@ CREATE TABLE "cities" (
     "state_name" varchar(50)   NOT NULL,
     "county_fips" int   NOT NULL,
     "county_name" varchar(50)   NOT NULL,
-    "lat" int   NOT NULL,
-    "lng" int   NOT NULL,
+    "lat" float   NOT NULL,
+    "lng" float   NOT NULL,
     "population" int   NOT NULL,
-    "density" int   NOT NULL,
+    "density" float   NOT NULL,
     "source" varchar(100)   NOT NULL,
     "military" boolean   NOT NULL,
     "incorporated" boolean   NOT NULL,
     "timezone" varchar(50)   NOT NULL,
     "ranking" int   NOT NULL,
     "zips" varchar(100)   NOT NULL,
-    "id" int   NOT NULL
+    "id" int   NOT NULL,
+	FOREIGN KEY ("city") REFERENCES restaurants("city"),
+	FOREIGN KEY ("state_name") REFERENCES restaurants("state")
 );
 
 ALTER TABLE "restaurants" ADD CONSTRAINT "fk_restaurants_city_state" FOREIGN KEY("city", "state")
 REFERENCES "cities" ("city", "state_name");
+
+SELECT * FROM cities;
+
+SELECT * FROM restaurants;
+
+DROP TABLE cities;
+
+DROP TABLE restaurants;
 
